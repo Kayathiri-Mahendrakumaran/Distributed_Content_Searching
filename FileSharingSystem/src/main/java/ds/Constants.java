e5274db1e75374b041037d4449c9a6597bd0cc86
Kayathiri-Mahendrakumaran <kayathirikaya33@gmail.com>
2021-02-16 14:06:29 +0530

    add result class

--- FileSharingSystem/src/main/java/ds/Constants.java
+++ FileSharingSystem/src/main/java/ds/Constants.java
@@ -3,4 +3,7 @@ package main.java.ds;
 public class Constants {
     public static final int MIN_NEIGHBOURS = 2;
     public static final int MAX_NEIGHBOURS = 6;
+
+    public static final int FTP_PORT_OFFSET = 100;
+
 }
---
+++ FileSharingSystem/src/main/java/ds/core/Result.java
@@ -0,0 +1,45 @@
+package main.java.ds.core;
+
+import main.java.ds.Constants;
+
+public class Result {
+    private String fileName;
+    private String location_address;
+    private int location_port;
+    private int tcpPort;
+    private int num_hops;
+    private long timeElapsed;
+
+    public Result(String fileName, String address, int port, int hops, long timeElapsed) {
+        this.fileName = fileName;
+        this.location_address = address;
+        this.location_port = port;
+        this.tcpPort = port + Constants.FTP_PORT_OFFSET;
+        this.num_hops = hops;
+        this.timeElapsed = timeElapsed;
+    }
+    //get filename
+    public String getFileName() {
+        return fileName;
+    }
+    //get location address
+    public String getAddress() {
+        return location_address;
+    }
+    //get location port
+    public int getPort() {
+        return location_port;
+    }
+    //get TCP port
+    public int getTcpPort() {
+        return tcpPort;
+    }
+    //get number of hops
+    public int getHops() {
+        return num_hops;
+    }
+    //get time taken
+    public long getTimeElapsed() {
+        return timeElapsed;
+    }
+}
